4d917bfde342b692cf912d20f01e99bee8225676
rahul patil <rahul.deshmukhpatil@gmail.com>
2022-02-20 23:34:58 +0000

    default commit message

--- install_zsh.sh
+++ install_zsh.sh
@@ -34,3 +34,9 @@ git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 
 #install ZSH syntax highlighting
 sudo aptitude -y install zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+
+git clone https://github.com/powerline/fonts.git --depth=1
+cd fonts && ./install.sh && cd ..
