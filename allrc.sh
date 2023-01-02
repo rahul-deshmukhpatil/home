fd567f517c6b72278c66f277e1f4c6c331db458b
igwmh10 <igw.mh10@gmail.com>
2023-01-02 17:44:42 +0000

    default commit message

--- allrc.sh
+++ allrc.sh
@@ -2,3 +2,4 @@ source ~/bitbucket/home/bashrc.sh
 source ~/bitbucket/home/pypyenv.sh
 source ~/bitbucket/home/condarc.sh
 source ~/bitbucket/home/vesperrc.sh
+source ~/bitbucket/home/custom_path_rc.sh
