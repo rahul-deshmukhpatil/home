1ea8e84eb31b8717a1b3e82af3fbb16368200856
rahul patil <rahul.deshmukhpatil@gmail.com>
2026-06-11 07:55:33 +0100

    -m added install basic

---
+++ install_basic.sh
@@ -0,0 +1,12 @@
+# generate ssh key
+ssh-keygen -t ed25519 -C "rahul.deshmukhpatil@gmail.com"
+
+sudo aptitude install git vim 
+sudo apt-get install aptitude
+sudo aptitude update
+sudo aptitude upgrade
+echo "source ~/bitbucket/home/allrc.sh" >> ~/.bashrc
+
+read -p "Download goolge chrome"
+sudo dpkg -i ~/Downloads/google-chrome-stable_current_amd64.deb 
+
